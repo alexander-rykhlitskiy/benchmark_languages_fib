dee5ad8da6d5c2098f8342ebb8954eec0b2dca32
Juan Carlos <juancarlospaco@gmail.com>
2018-11-06 14:26:15 -0300

    Update fibonachi.nim
    Take arg from command line param.
    for loop 0 to arg.

--- fibonachi.nim
+++ fibonachi.nim
@@ -1,5 +1,8 @@
-func fib(n: uint64): uint64 =
+import os, strutils
+
+func fib(n: uint64): uint64 {.inline.} =
   if n <= 1: return 1
   return fib(n - 1) + fib(n - 2)
 
-echo fib(46)
+for i in 0..paramStr(1).parseint.uint64:
+  echo fib(i)
