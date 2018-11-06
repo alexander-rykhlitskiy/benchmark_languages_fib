bc82f4b137d077af6ededa66b5a20a72881213e8
Juan Carlos <juancarlospaco@gmail.com>
2018-11-06 15:50:21 -0300

    Create fibonachi.nim (#2)
    * Create fibonachi.nim
    
    Compile with:
    
    ```
    nim c -d:release --opt:speed --passL:-s fibonachi.nim
    ```
    
    Needs [Nim installed with ChooseNim](https://nim-lang.org/install_unix.html).
    :cat:
    
    * Update fibonachi.nim
    
    Take arg from command line param.
    for loop 0 to arg.
    
    * Update fibonachi.nim
    
    Print like "CurrentNumber-CurrentResult" on the terminal.
    
    * if arg use arg else use `38`.
    
    * Add Nim to README
    
    * Style NEP1
    
    * Replace "N-N" with "N - N" on print to terminal
    
    * return n

---
+++ fibonachi.nim
@@ -0,0 +1,9 @@
+import os, strutils
+
+func fib(n: uint64): uint64 {.inline.} =
+  if n <= 1: return n
+  return fib(n - 1) + fib(n - 2)
+
+let n = if paramCount() == 1: paramStr(1).parseInt.uint64 else: 38
+for i in 0..n:
+  echo i, " - ", fib(i)
