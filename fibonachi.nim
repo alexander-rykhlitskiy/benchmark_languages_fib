c7520ab89cde31e0cda9a6492d3096b2f7ec21a9
Juan Carlos <juancarlospaco@gmail.com>
2018-11-06 15:37:24 -0300

    Style NEP1

--- fibonachi.nim
+++ fibonachi.nim
@@ -4,6 +4,6 @@ func fib(n: uint64): uint64 {.inline.} =
   if n <= 1: return 1
   return fib(n - 1) + fib(n - 2)
 
-let n = if paramCount() == 1: paramStr(1).parseint.uint64 else: 38
+let n = if paramCount() == 1: paramStr(1).parseInt.uint64 else: 38
 for i in 0..n:
   echo i, '-', fib(i)
